d6888e427a697be4ac8bb839c7a058aff4892bda
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-11-09 19:37:31 +0900

    chore: localhost https 적용 및 사용자 운영체제마다 다르게 mkcert 설치하는 방법 설정 -> pull 받고 npm run init-https 실행 후 원래 하던대로 npm run dev해서 https 적용되는지 확인 바람

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -50,6 +50,10 @@ export default defineConfig(({mode}) => {
           target: env.VITE_API_URL,
           changeOrigin: true,
         },
+      },
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+        cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
       },
     },
   };
