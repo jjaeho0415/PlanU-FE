d20ece1a1a60568553291099023df9cd283191ce
shuding0307 <suhyun2116@gmail.com>
2024-11-18 10:12:01 +0900

    LoginButton Component

--- src/pages/StartPage/index.tsx
+++ src/pages/StartPage/index.tsx
@@ -2,19 +2,15 @@ import styles from "./startPage.module.scss";
 import React from "react";
 import Logo from '@assets/logo/planU로고.svg?react';
 import Kakao from "@assets/logo/kakaoLogin.svg?react";
+import LoginButton from "@components/buttons/LoginButton";
 
 const StartPage: React.FC = () => {
   return (
     <div className={styles.Container}>
       <Logo />
-      <div className={styles.InputBox}>
-        <input className={styles.Input} placeholder="ID" />
-        <input className={styles.Input} placeholder="Password" />
-      </div>
-      <div className={styles.BottomButtonBox}>
-        <p className={styles.Cursor}>로그인</p>
-        <p>|</p>
-        <p className={styles.Cursor}>회원가입</p>
+      <div className={styles.ButtonBox}>
+        <LoginButton buttonType="login_kakao" onClick={()=>{return;}}/>
+        <LoginButton buttonType="login_other" onClick={()=>{return;}}/>
       </div>
       <div className={styles.EasyLoginBox}>
         <p className={styles.EasyLoginTitle}>간편 로그인</p>
