cd30aeb8a71334742f536d71e16772067547551a
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-11-22 20:39:23 +0900

    style: 버튼 스토리북 파일에 스타일 추가

--- src/components/buttons/Button.stories.tsx
+++ src/components/buttons/Button.stories.tsx
@@ -4,64 +4,73 @@ import { GoLogin } from "./GoLogin";
 import LoginButton from "./LoginButton";
 import SmallButton from "./SmallButton";
 import { MemoryRouter } from "react-router-dom";
+import styles from "./story.module.scss";
 
 function Button() {
   return (
     <>
       <div>
         <h1>DefaultButton</h1>
-        <DefaultButton
-          buttonText="완료"
-          onClick={() => {
-            return;
-          }}
-        />
+        <div className={styles.Container}>
+          <DefaultButton
+            buttonText="완료"
+            onClick={() => {
+              return;
+            }}
+          />
+        </div>
       </div>
 
       <div>
         <h1>GoLogin</h1>
-        <GoLogin textType="로그인" />
-        <GoLogin textType="회원가입" />
+        <div className={styles.Container}>
+          <GoLogin textType="로그인" />
+          <GoLogin textType="회원가입" />
+        </div>
       </div>
 
       <div>
         <h1>LoginButton</h1>
-        <LoginButton
-          buttonType="login"
-          onClick={() => {
-            alert("로그인");
-          }}
-        />
-        <LoginButton
-          buttonType="login_kakao"
-          onClick={() => {
-            alert("카카오톡으로 로그인");
-          }}
-        />
-        <LoginButton
-          buttonType="login_kakao_white"
-          onClick={() => {
-            alert("카카오 로그인");
-          }}
-        />
-        <LoginButton
-          buttonType="login_other"
-          onClick={() => {
-            alert("다른 방법으로 로그인");
-          }}
-        />
-        <LoginButton
-          buttonType="register"
-          onClick={() => {
-            alert("회원가입");
-          }}
-        />
+        <div className={styles.Container}>
+          <LoginButton
+            buttonType="login"
+            onClick={() => {
+              alert("로그인");
+            }}
+          />
+          <LoginButton
+            buttonType="login_kakao"
+            onClick={() => {
+              alert("카카오톡으로 로그인");
+            }}
+          />
+          <LoginButton
+            buttonType="login_kakao_white"
+            onClick={() => {
+              alert("카카오 로그인");
+            }}
+          />
+          <LoginButton
+            buttonType="login_other"
+            onClick={() => {
+              alert("다른 방법으로 로그인");
+            }}
+          />
+          <LoginButton
+            buttonType="register"
+            onClick={() => {
+              alert("회원가입");
+            }}
+          />
+        </div>
       </div>
 
       <div>
         <h1>SmallButton</h1>
-        <SmallButton buttonText="확인" color="default" />
-        <SmallButton buttonText="확인" color="light" />
+        <div className={styles.Container}>
+          <SmallButton buttonText="확인" color="default" />
+          <SmallButton buttonText="확인" color="light" />
+        </div>
       </div>
     </>
   );
