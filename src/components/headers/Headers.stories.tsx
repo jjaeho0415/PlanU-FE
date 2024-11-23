6d1326edcd1464ebdaec582320885d4f0d9f5f9a
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-11-23 18:09:20 +0900

    style: 헤더 스토리북 코드에 HasOnlyBackArrowHeader 컴포넌트 작성

---
+++ src/components/headers/Headers.stories.tsx
@@ -0,0 +1,84 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { MemoryRouter } from "react-router-dom";
+import styles from "./headerStory.module.scss";
+import HasOnlyBackArrowHeader from "./HasOnlyBackArrowHeader";
+import ChattingHeader from "./ChattingHeader";
+import CalenderHeader from "./CalenderHeader";
+import HasTwoIconHeader from "./HasTwoIconHeader";
+import HasOnlyRightIconHeader from "./HasOnlyRightIconHeader";
+
+function Headers() {
+  return (
+    <>
+      <div>
+        <h1>HasOnlyBackArrowHeader</h1>
+        <div className={styles.Container}>
+          <HasOnlyBackArrowHeader
+            title="가입하기"
+            handleClick={() => {
+              return;
+            }}
+          />
+          <HasOnlyBackArrowHeader
+            title="가능한 날짜 선택"
+            handleClick={() => {
+              return;
+            }}
+          />
+        </div>
+      </div>
+
+      <div>
+        <h1>CalenderHeader</h1>
+        <div className={styles.Container}>
+          <CalenderHeader />
+          <CalenderHeader />
+        </div>
+      </div>
+
+      <div>
+        <h1>ChattingHeader</h1>
+        <div className={styles.Container}>
+          <ChattingHeader />
+        </div>
+      </div>
+
+      <div>
+        <h1>HasTwoIconHeader</h1>
+        <div className={styles.Container}>
+          <HasTwoIconHeader />
+          <HasTwoIconHeader />
+          <HasTwoIconHeader />
+        </div>
+      </div>
+
+      <div>
+        <h1>HasOnlyRightIconHeader</h1>
+        <div className={styles.Container}>
+          <HasOnlyRightIconHeader />
+          <HasOnlyRightIconHeader />
+        </div>
+      </div>
+    </>
+  );
+}
+
+const meta: Meta<typeof Headers> = {
+  title: "Headers",
+  component: Headers,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Headers>;
+
+export const Default: Story = {
+  args: {},
+};
