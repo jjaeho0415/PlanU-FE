dd8277f0b5c7591dde342a727248b83a334f7d55
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-11-25 16:49:48 +0900

    style:회원정보 등록 페이지 퍼블리싱 작업 중

--- src/pages/RegisterAccountPage/page/index.tsx
+++ src/pages/RegisterAccountPage/page/index.tsx
@@ -1,9 +1,36 @@
 import styles from "./registerAccount.module.scss";
+import ProfileEdit_Icon from "@assets/Icons/Icon_ProfileImageEdit.svg?react";
+import DefaultProfile_Icon from "@assets/Icons/Icon_DefaultProfile.svg?react";
+import OnlyTextHeader from "@components/headers/OnlyTextHeader";
+import { useState } from "react";
 
 const RegisterAccountPage = () => {
+  const [userBirth, setUserBirth] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const handleBirthOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserBirth(e.target.value);
+  };
+
   return (
     <div className={styles.mainContainer}>
-      <div></div>
+      <OnlyTextHeader title="회원 등록" backgroundColor="purple" />
+      <div className={styles.contentContainer}>
+        <div className={styles.topSection}>
+          <div className={styles.profileImageSection}>
+            <DefaultProfile_Icon width={130} height={130} />
+            <ProfileEdit_Icon width={24} height={24} className={styles.profileEditIcon} />
+          </div>
+          <div>이수현 님</div>
+        </div>
+        <div className={styles.middleSection}>
+          <div className={styles.birthSection}>
+            <div>생년월일</div>
+            <input value={userBirth} onChange={handleBirthOnChange} className={styles.birthInput} />
+            {errorMessage && <div className={styles.errorMessage}>{errorMessage}</div>}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
