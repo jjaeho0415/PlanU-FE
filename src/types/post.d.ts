764d604fe67c88ddd2c6c97fac21c7c36705b07a
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-12-04 18:40:20 +0900

    chore: 회원정보등록 api에서 사용되는 요청 데이터 타입 types/post.d.ts에 정의

--- src/types/post.d.ts
+++ src/types/post.d.ts
@@ -2,3 +2,16 @@ type IPostConfirmEmailCode = {
   email: string;
   verificationCode: string;
 };
+
+type IPostUserInformationType = {
+  UserProfileRequest: {
+    birthDate: string;
+    gender: string;
+    profileImage: File | null;
+  };
+  TermsRequest: {
+    isPrivacyPolicyAgreed: string;
+    isTermsOfServiceAgreed: string;
+    isSnsReceiveAgreed: string;
+  };
+};
