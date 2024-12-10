6b972a3672e3b8ad2617c9966af82115011a14bd
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-12-10 17:06:51 +0900

    fix: 회원정보등록 여부 확인 api에서 중복로직 삭제

--- src/api/user/getIsExistUserProfile.ts
+++ src/api/user/getIsExistUserProfile.ts
@@ -2,14 +2,9 @@ import apiRoutes from "@api/apiRoutes";
 import api from "@api/fetcher";
 
 export const getIsExistUserProfile = async (authorization: string): Promise<string> => {
-  try {
-    const response = await api.get<IResponseType>({
-      endpoint: apiRoutes.userInformationExist,
-      authorization,
-    });
-    return response.resultMsg;
-  } catch (error) {
-    console.error(error);
-    throw new Error("유효한 액세스 토큰이 아닙니다");
-  }
+  const response = await api.get<IResponseType>({
+    endpoint: apiRoutes.userInformationExist,
+    authorization,
+  });
+  return response.resultMsg;
 };
