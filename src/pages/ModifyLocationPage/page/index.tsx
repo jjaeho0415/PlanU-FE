46cc97fadf5d8503009d6a6541f0b2bba1c59ac1
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-12-18 17:23:59 +0900

    chore: 피그마를 기반으로 누락된 페이지 추가 및 필요없는 페이지 삭제 후 라우팅 재설정

--- src/pages/ModifyLocationPage/page/index.tsx
+++
@@ -1,5 +0,0 @@
-const ModifyLocationPage: React.FC = () => {
-  return <div>ModifyLocationPage</div>;
-};
-
-export default ModifyLocationPage;
---
+++ src/pages/SelectLocationPage/page/index.tsx
@@ -0,0 +1,5 @@
+const SelectLocationPage: React.FC = () => {
+  return <div>SelectLocationPage</div>;
+};
+
+export default SelectLocationPage;
