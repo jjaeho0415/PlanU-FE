8eb0028159aa1d71427b80e979f044c1b882111a
shuding0307 <suhyun2116@gmail.com>
2024-12-22 22:23:39 +0900

    하단 버튼 추가

--- src/pages/FindPage/page/index.tsx
+++ src/pages/FindPage/page/index.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./find.module.scss";
 import HasOnlyBackArrowHeader from "@components/headers/HasOnlyBackArrowHeader";
 import { useNavigate } from "react-router-dom";
 import FindTypeTab from "../components/FindTypeTab";
-import FindButton from "@components/buttons/FindButton";
 import FindInput from "../components/FindInput";
+import { DefaultButton } from "@components/buttons/DefaultButton";
 
 const FindPage: React.FC = () => {
   const navigate = useNavigate();
   const [selectedTab, setSelectedTab] = useState<"id" | "pw">("id");
+  const [buttonText, setButtonText] = useState<string>();
+
+  useEffect(() => {
+    switch (selectedTab) {
+      case "id":
+        setButtonText("아이디 찾기");
+        break;
+      case "pw":
+        setButtonText("비밀번호 변경");
+    }
+  });
 
   return (
     <div className={styles.Container}>
@@ -19,6 +30,14 @@ const FindPage: React.FC = () => {
       />
       <FindTypeTab selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <FindInput findType={selectedTab} />
+      <div className={styles.ButtonBox}>
+        <DefaultButton
+          buttonText={selectedTab === "id" ? "아이디 찾기" : "비밀번호 찾기"}
+          onClick={() => {
+            return;
+          }}
+        />
+      </div>
     </div>
   );
 };
