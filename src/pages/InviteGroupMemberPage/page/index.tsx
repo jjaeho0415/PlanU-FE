f4eac1cd98f0b79f7f5d75305b9ea153b9700c6d
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-12-30 15:08:45 +0900

    style: 멤버 추가 페이지 퍼블리싱 중

--- src/pages/InviteGroupMemberPage/page/index.tsx
+++ src/pages/InviteGroupMemberPage/page/index.tsx
@@ -1,6 +1,28 @@
+import HasOnlyBackArrowHeader from "@components/headers/HasOnlyBackArrowHeader"
+import styles from "./inviteGroupMember.module.scss"
+import { useNavigate } from "react-router-dom"
+import SearchBox from "../components/SearchBox";
+import { useState } from "react";
+
 const InviteGroupMemberPage = () => {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState<string>("");
+  const handleSearchIconClick = () => {
+    // 검색 api 호출 로직 작성
+    return;
+  }
+
   return (
-    <div>InviteGroupMemberPage</div>
+    <div className={styles.mainContainer}>
+      <HasOnlyBackArrowHeader title="멤버 초대" handleClick={() => navigate(-1)}/>
+      <div className={styles.contentContainer}>
+        <div className={styles.searchContainer}>
+          <SearchBox inputValue={inputValue} setInputValue={setInputValue} type="invite" handleSearchIconClick={handleSearchIconClick}/>
+        </div>
+        <div className={styles.line}/>
+        <div className={styles.friendContainer}></div>
+      </div>
+    </div>
   )
 }
 
