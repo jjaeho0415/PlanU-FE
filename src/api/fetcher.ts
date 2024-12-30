a333ff50b66e2660d09c3a13a9c29828126f242c
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2024-12-30 15:08:15 +0900

    style: 멤버 추가 페이지의 search Input 컴포넌트 퍼블리싱

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -81,7 +81,6 @@ const _fetch = async <T = unknown, R = unknown>({
           useAuthStore.getState().setIsLogin(false);
           useAuthStore.getState().setAccessToken("");
           useBottomStore.getState().setBottomIndex(0);
-          // ProtectedRoute 구현하면 window.location.reload로 수정해야함
           window.location.href = `${window.location.origin}`;
           throw new Error("Session expired. Please log in again.");
         }
