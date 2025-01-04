47e0c568d5606843959daadbc5033ed857c7b360
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-01-04 16:44:40 +0900

    style: 그룹 멤버 목록 페이지 퍼블리싱

--- src/pages/GroupMemberPage/page/index.tsx
+++ src/pages/GroupMemberPage/page/index.tsx
@@ -1,8 +1,56 @@
 import HasOnlyBackArrowHeader from "@components/headers/HasOnlyBackArrowHeader";
 import { useNavigate } from "react-router-dom";
-import styles from "./groupMember.module.scss"
+import styles from "./groupMember.module.scss";
 import { useState } from "react";
 import SearchBox from "@pages/InviteGroupMemberPage/components/SearchBox";
+import MemberList from "../components/memberList";
+
+const groupMembers: IGetGroupMembersType = {
+  members: [
+    {
+      name: "이수현",
+      username: "shuding0307",
+      groupRole: "PARTICIPANT",
+      friendStatus: "FRIEND",
+      profileImageUrl: "",
+    },
+    {
+      name: "이다은",
+      username: "Euniii0713",
+      groupRole: "PARTICIPANT",
+      friendStatus: "NOT_FRIEND",
+      profileImageUrl: "",
+    },
+    {
+      name: "이상준",
+      username: "sang__00",
+      groupRole: "PARTICIPANT",
+      friendStatus: "NOT_FRIEND",
+      profileImageUrl: "",
+    },
+    {
+      name: "김도하",
+      username: "ehgk4245",
+      groupRole: "PARTICIPANT",
+      friendStatus: "FRIEND",
+      profileImageUrl: "",
+    },
+    {
+      name: "최준혁",
+      username: "_twinkle_high",
+      groupRole: "PARTICIPANT",
+      friendStatus: "RECEIVE",
+      profileImageUrl: "",
+    },
+    {
+      name: "정재호",
+      username: "purify_0kcal",
+      groupRole: "LEADER",
+      friendStatus: "ME",
+      profileImageUrl: "",
+    },
+  ],
+};
 
 const GroupMemberPage: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
@@ -15,7 +63,7 @@ const GroupMemberPage: React.FC = () => {
 
   return (
     <div className={styles.mainContainer}>
-      <HasOnlyBackArrowHeader title="멤버 초대" handleClick={() => navigate(-1)} />
+      <HasOnlyBackArrowHeader title="멤버" handleClick={() => navigate(-1)} />
       <div className={styles.contentContainer}>
         <div className={styles.searchContainer}>
           <SearchBox
@@ -24,6 +72,13 @@ const GroupMemberPage: React.FC = () => {
             type="invite"
             handleSearchIconClick={handleSearchIconClick}
           />
+        </div>
+        <div className={styles.memberListContainer}>
+          <div className={styles.textBox}>멤버목록</div>
+          <div className={styles.emptyBox} />
+          <div className={styles.scrollContainer}>
+            <MemberList memberList={groupMembers.members} />
+          </div>
         </div>
       </div>
     </div>
