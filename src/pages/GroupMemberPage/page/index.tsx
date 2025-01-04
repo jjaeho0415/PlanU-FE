e3ac37319e5ca556cac6544467e2e4de3b4da207
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-01-04 13:23:53 +0900

    style: 그룹 멤버 목록 페이지 퍼블리싱중

--- src/pages/GroupMemberPage/page/index.tsx
+++ src/pages/GroupMemberPage/page/index.tsx
@@ -1,5 +1,33 @@
+import HasOnlyBackArrowHeader from "@components/headers/HasOnlyBackArrowHeader";
+import { useNavigate } from "react-router-dom";
+import styles from "./groupMember.module.scss"
+import { useState } from "react";
+import SearchBox from "@pages/InviteGroupMemberPage/components/SearchBox";
+
 const GroupMemberPage: React.FC = () => {
-  return <div>GroupMemberPage</div>;
+  const [inputValue, setInputValue] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleSearchIconClick = () => {
+    // 검색 api 호출 로직 작성
+    return;
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <HasOnlyBackArrowHeader title="멤버 초대" handleClick={() => navigate(-1)} />
+      <div className={styles.contentContainer}>
+        <div className={styles.searchContainer}>
+          <SearchBox
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            type="invite"
+            handleSearchIconClick={handleSearchIconClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GroupMemberPage;
