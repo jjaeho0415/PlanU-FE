86edef9c4b0fcf13654ce54174f23427fe1db658
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-01-11 19:12:01 +0900

    style: 일정 생성 페이지에서 사용되는 지도 컴포넌트 퍼블리싱 및 기능 구현

--- src/components/map/Map.stories.tsx
+++ src/components/map/Map.stories.tsx
@@ -3,19 +3,24 @@ import { MemoryRouter } from "react-router-dom";
 import Map from "./Map";
 
 function Map_Story() {
-  const selectedLocationInfo: ILocationInfoType = {
-    location: "강원도 춘천시 백령로 61번길 4",
-    longitude: "37.869159768232095",
-    latitude: "127.73680043244275",
+  // const selectedLocationInfo: UserLatLngType = {
+  //   lat: 37.557169,
+  //   lng: 126.923834,
+  // };
+  const notSelectedLocationInfo: UserLatLngType = {
+    lng: 0,
+    lat: 0,
   };
 
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "50px" }}>
-      <div style={{ width: "351px", height: "436px", border: "1px solid #CB96D9" }}>
-        <Map />
-      </div>
-      <div style={{ width: "351px", height: "436px", border: "1px solid #CB96D9" }}>
-        <Map selectedLocationInfo={selectedLocationInfo} />
+      {/* <div>선택된 위치가 있을때(홍대입구역 2번출구)</div>
+      <div style={{ width: "351px", height: "201px", border: "1px solid #CB96D9" }}>
+        <Map latLng={selectedLocationInfo} />
+      </div> */}
+      <div>선택된 위치가 없을때(현재위치)</div>
+      <div style={{ width: "351px", height: "201px", border: "1px solid #CB96D9" }}>
+        <Map latLng={notSelectedLocationInfo} />
       </div>
     </div>
   );
--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,11 +1,64 @@
+import { loadGoogleMapsAPI } from "@api/googleMapLoader";
 import styles from "./map.module.scss";
+import { useEffect, useRef, useState } from "react";
 
-interface Props{
-    selectedLocationInfo?: ILocationInfoType;
+interface Props {
+  latLng: UserLatLngType;
 }
 
-const Map: React.FC<Props> = ({ selectedLocationInfo }) => {
-  return <div className={styles.mapContainer}>Map</div>;
+const Map: React.FC<Props> = ({ latLng }) => {
+  const [userLatLng, setUserLatLng] = useState<UserLatLngType>();
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    setUserLatLng({
+      lat: latLng.lat,
+      lng: latLng.lng,
+    });
+  }, [latLng]);
+
+  useEffect(() => {
+    async function initMap() {
+      try {
+        await loadGoogleMapsAPI(import.meta.env.VITE_GOOGLE_MAP_API_KEY);
+        const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+          "marker",
+        )) as google.maps.MarkerLibrary;
+        if (userLatLng?.lat === 0 && userLatLng?.lng === 0) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              setUserLatLng({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              });
+            },
+
+            (error) => {
+              console.error("Error getting user's location: ", error);
+            },
+          );
+        }
+
+        const map = new google.maps.Map(mapRef.current as HTMLElement, {
+          center: userLatLng,
+          zoom: 16,
+          mapId: import.meta.env.VITE_GOOGLE_MAP_ID,
+        });
+
+        new AdvancedMarkerElement({
+          map,
+          position: userLatLng,
+        });
+      } catch (error) {
+        console.error("Error initializing map: ", error);
+      }
+    }
+    if (mapRef.current && userLatLng) {
+      initMap();
+    }
+  }, [userLatLng]);
+
+  return <div className={styles.mapContainer} ref={mapRef} />;
 };
 
-export default Map
+export default Map;
