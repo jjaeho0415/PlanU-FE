88582cf32d10337f8c68c3f23b156cb977285bb2
shuding0307 <suhyun2116@gmail.com>
2025-01-18 18:01:30 +0900

    style: 그룹 달력 페이지 퍼블리싱

--- src/pages/GroupCalendarPage/page/index.tsx
+++ src/pages/GroupCalendarPage/page/index.tsx
@@ -1,5 +1,61 @@
+import EditIcon from "@assets/Icons/myCalendar/EditIcon.svg?react";
+import React, { useState } from "react";
+import Calendar from "../../../components/calendar/Calendar";
+import CalendarHeader from "../../../components/headers/CalendarHeader";
+import Footer from "../../../components/nav-bar/BottomNavBar";
+import styles from "./groupCalendarPage.module.scss";
+import EventCard from "@components/calendarPage/EventCard";
+
+interface IGetScheduleType {
+  date: string;
+  isSchedule: boolean;
+  isBirthday: boolean;
+}
+
 const GroupCalendarPage: React.FC = () => {
-  return <div>GroupCalendarPage</div>;
+  const [availableDates, setAvailableDates] = useState<string[]>([]);
+
+  const scheduleData: IGetScheduleType[] = [
+    { date: "2025-01-04", isSchedule: true, isBirthday: false },
+    { date: "2025-01-13", isSchedule: false, isBirthday: true },
+    { date: "2025-01-16", isSchedule: true, isBirthday: true },
+    { date: "2025-01-26", isSchedule: true, isBirthday: false },
+  ];
+
+  const handleBackArrowClick = () => console.log();
+  const handleMiniCalendarClick = () => console.log();
+
+  return (
+    <div className={styles.Container}>
+      <CalendarHeader
+        title="그룹 달력"
+        type="group"
+        handleBackArrowClick={handleBackArrowClick}
+        handleMiniCalendarClick={handleMiniCalendarClick}
+      />
+      <div className={styles.content}>
+        <div className={styles.calendarSection}>
+          <Calendar
+            type="view"
+            availableDates={availableDates}
+            setAvailableDates={setAvailableDates}
+            scheduleData={scheduleData}
+          />
+        </div>
+        <div className={styles.scheduleSection}>
+          <div className={styles.scheduleHeaderContainer}>
+            <h1 className={styles.scheduleHeader}>1월 16일 (목)</h1>
+            <EditIcon className={styles.editIcon} />
+          </div>
+          <EventCard
+            time="19:00 ~ 20:00"
+            title="수현이의 생일파티"
+            location="홍대입구역 2번 출구 앞"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GroupCalendarPage;
