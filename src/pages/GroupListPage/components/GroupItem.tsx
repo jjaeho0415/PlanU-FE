300ff8fecc3e0ac9d7d656525dbe60bfd74680fd
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-01-19 13:58:59 +0900

    fix: 그룹 목록 페이지에서 사용되는 groupItem 컴포넌트 수정

--- src/pages/GroupListPage/components/GroupItem.tsx
+++ src/pages/GroupListPage/components/GroupItem.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import styles from "./groupItem.module.scss";
-import DefaultProfileImage from "@assets/Icons/Default Profile/default_profile.svg?react";
 
-interface IProps {}
+interface IProps {
+  groupItem: IGetGroupListItemType;
+}
 
-const GroupItem: React.FC<IProps> = () => {
+const GroupItem: React.FC<IProps> = ({ groupItem }) => {
   return (
     <div className={styles.Container}>
       <div className={styles.ProfileImg}>
-        <DefaultProfileImage />
+        <img
+          src={groupItem.groupImageUrl}
+          alt={`${groupItem.groupName}의 프로필`}
+          className={styles.image}
+        />
       </div>
       <div className={styles.InfoBox}>
-        <p className={styles.GroupName}>planU 수다방</p>
-        <p className={styles.MembersNum}>• 7명 참여 중</p>
+        <p className={styles.GroupName}>{groupItem.groupName}</p>
+        <p className={styles.MembersNum}>• {groupItem.participant}명 참여 중</p>
       </div>
     </div>
   );
