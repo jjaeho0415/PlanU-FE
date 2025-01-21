f9fd40b2c1b7f5b5acb05f0b2809706ea6bd5e46
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-01-21 22:19:41 +0900

    feat: 친구 요청 거절 api 연동

---
+++ src/api/friend/deleteRejectRequestFriend.ts
@@ -0,0 +1,24 @@
+import apiRoutes from "@api/apiRoutes";
+import api from "@api/fetcher";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const deleteRejectRequestFriend = async (authorization: string, username: string) => {
+  const response: IResponseType = await api.delete({
+    endpoint: `${apiRoutes.rejectFriendRequest}?username=${username}`,
+    authorization,
+  });
+  return response;
+};
+
+export const useDeleteRejectRequestFriend = (authorization: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (username: string) => deleteRejectRequestFriend(authorization, username),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["GROUP_MEMBER_LIST"],
+      });
+    },
+    onError: (error) => alert(error.message),
+  });
+};
