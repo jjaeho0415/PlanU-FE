388d7287a177330485aa7e34af5f750ee9d66806
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-02-01 20:49:54 +0900

    Feat: 그룹 상대 추가 목록 api, 그룹 멤버 초대 api 연동

--- src/pages/InviteGroupMemberPage/page/index.tsx
+++ src/pages/InviteGroupMemberPage/page/index.tsx
@@ -1,59 +1,27 @@
 import HasOnlyBackArrowHeader from "@components/headers/HasOnlyBackArrowHeader";
 import styles from "./inviteGroupMember.module.scss";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import SearchBox from "../components/SearchBox";
 import { useState } from "react";
 import FriendList from "../components/FriendList";
-
-const friendMemberList: IGetFriendMemberType[] = [
-  {
-    profileImage: "",
-    name: "정재호",
-    username: "jjh",
-    status: "NONE",
-  },
-  {
-    profileImage: "",
-    name: "김도하",
-    username: "kdh",
-    status: "NONE",
-  },
-  {
-    profileImage: "",
-    name: "이다은",
-    username: "lde",
-    status: "NONE",
-  },
-  {
-    profileImage: "",
-    name: "이수현",
-    username: "lsh",
-    status: "NONE",
-  },
-  {
-    profileImage: "",
-    name: "이상준",
-    username: "lsj",
-    status: "NONE",
-  },
-  {
-    profileImage: "",
-    name: "최준혁",
-    username: "cjh",
-    status: "PROGRESS",
-  },
-];
+import useAuthStore from "@store/useAuthStore";
+import { useGetGroupMemberInviteList } from "@api/group/getGroupMemberInviteList";
+import { usePostInviteGroupMember } from "@api/group/postInviteGroupMember";
 
 const InviteGroupMemberPage = () => {
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState<string>("");
+  const { groupId } = useParams<{ groupId: string }>();
+  const { accessToken } = useAuthStore.getState();
+  const { data: groupMemberInviteList } = useGetGroupMemberInviteList(groupId!, accessToken);
+  const { mutate: inviteGroupMember } = usePostInviteGroupMember(groupId!, accessToken)
 
-  const handleInviteGroupMemberClick = () => {
-    // 그룹 참여 요청 api 연동
+  const handleInviteGroupMemberClick = (username: string) => {
+    inviteGroupMember(username);
     return;
   };
   const handleCancelInviteClick = () => {
-    // 그룹 참여 요청 취소 요청 api 연동
+    // 그룹 초대 취소 api 연동
     return;
   };
   const handleSearchIconClick = () => {
@@ -76,11 +44,13 @@ const InviteGroupMemberPage = () => {
         <div className={styles.line}></div>
         <div className={styles.friendContainer}>
           <div className={styles.friendText}>친구</div>
-          <FriendList
-            friendList={friendMemberList}
-            handleInviteGroupMemberClick={handleInviteGroupMemberClick}
-            handleCancelInviteClick={handleCancelInviteClick}
-          />
+          {groupMemberInviteList && (
+            <FriendList
+              friendList={groupMemberInviteList.nonGroupFriends}
+              handleInviteGroupMemberClick={handleInviteGroupMemberClick}
+              handleCancelInviteClick={handleCancelInviteClick}
+            />
+          )}
         </div>
       </div>
     </div>
