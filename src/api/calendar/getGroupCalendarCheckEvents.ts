0e4bda15e025c30d267833d20327202df09b5b0c
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-02-24 17:45:42 +0900

    feat: 그룹 달력 페이지 그룹 일정 유무 조회 api 연동 로직 수정 및 그룹 날짜 일정 조회 api 연동

--- src/api/calendar/getGroupCalendarCheckEvents.ts
+++ src/api/calendar/getGroupCalendarCheckEvents.ts
@@ -1,21 +1,27 @@
-import apiRoutes from "@api/apiRoutes"
-import api from "@api/fetcher"
+import apiRoutes from "@api/apiRoutes";
+import api from "@api/fetcher";
 import { useQuery } from "@tanstack/react-query";
 
-const getGroupCalendarCheckEvents = async (groupId: string, yearMonth: string, authorization: string) => {
-    const response =
-      api.get <
-      IGetGroupCalendarCheckEventsResponseBodyType>({
-        endpoint: `${apiRoutes.group}/${groupId}/schedules/check-events?yearMonth=${yearMonth}`,
-        authorization,
-      });
-    return response
-}
+const getGroupCalendarCheckEvents = async (
+  groupId: string,
+  yearMonth: string,
+  authorization: string,
+) => {
+  const response = api.get<IGetGroupCalendarCheckEventsResponseBodyType>({
+    endpoint: `${apiRoutes.group}/${groupId}/schedules/check-events?yearMonth=${yearMonth}`,
+    authorization,
+  });
+  return response;
+};
 
-export const useGetGroupCalendarCheckEvents = (groupId: string, yearMonth: string, authorization: string) => {
-    return useQuery({
-        queryKey: ["GROUP_CHECK_EVENTS", yearMonth],
-        queryFn: () => getGroupCalendarCheckEvents(groupId, yearMonth, authorization),
-        enabled: authorization !== null
-    })
-}
+export const useGetGroupCalendarCheckEvents = (
+  groupId: string,
+  yearMonth: string,
+  authorization: string,
+) => {
+  return useQuery({
+    queryKey: ["GROUP_CHECK_EVENTS", groupId, yearMonth],
+    queryFn: () => getGroupCalendarCheckEvents(groupId, yearMonth, authorization),
+    enabled: groupId !== undefined && authorization !== null,
+  });
+};
