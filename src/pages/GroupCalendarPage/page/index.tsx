2e52e58f9e893eeeba5799b6641f0a372acc1a25
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-02-24 17:48:00 +0900

    fix: 달력 페이지에서 사용되는 달력 컴포넌트 로직 수정 및 그룹 달력 페이지 Api 연동, 그룹 가능한 날짜 보기 페이지 로직 수정

--- src/pages/GroupCalendarPage/page/index.tsx
+++ src/pages/GroupCalendarPage/page/index.tsx
@@ -8,60 +8,25 @@ import { useNavigate, useParams } from "react-router-dom";
 import useAuthStore from "@store/useAuthStore";
 import { useGetGroupCalendarCheckEvents } from "@api/calendar/getGroupCalendarCheckEvents";
 import { format } from "date-fns";
-
-const scheduleList: IGetScheduleListResponseBodyType = {
-  schedules: [
-    {
-      id: 1,
-      groupId: 1,
-      title: "Weekly Meeting",
-      location: "Library Room A",
-      startTime: "10:00",
-      endTime: "12:00",
-      color: "#FF5733",
-    },
-    {
-      id: 1,
-      groupId: null,
-      title: "회의 일정",
-      location: "강원 춘천시 백령로 51",
-      startTime: "10:00",
-      endTime: "12:59",
-      color: "#FFA500",
-    },
-    {
-      id: 2,
-      groupId: null,
-      title: "프로젝트 회의",
-      location: "강원 춘천시 백령로 51",
-      startTime: "13:00",
-      endTime: "15:59",
-      color: "#FFA500",
-    },
-    {
-      id: 2,
-      groupId: 1,
-      title: "Project Discussion",
-      location: "Conference Room B",
-      startTime: "14:00",
-      endTime: "15:30",
-      color: "#33FF57",
-    },
-  ],
-  birthdayPerson: ["최준혁", "김도하"],
-};
+import { useGetGroupScheduleList } from "@api/calendar/getGroupScheduleList";
+import { ko } from "date-fns/locale";
 
 const GroupCalendarPage: React.FC = () => {
   const navigate = useNavigate();
   const { groupId } = useParams<{ groupId: string }>();
   const { accessToken } = useAuthStore.getState();
+  const currentDate = new Date();
   const [currentMonth, setCurrentMonth] = useState<Date>(new Date());
+  const [selectedDate, setSelectedDate] = useState<string>(format(currentDate, "yyyy-MM-dd"));
 
   const { data: groupCheckEvents } = useGetGroupCalendarCheckEvents(
     groupId!,
     format(currentMonth, "yyyy-MM"),
     accessToken,
   );
+
+  const { data: groupScheduleList } = useGetGroupScheduleList(groupId!, accessToken, selectedDate)
+  const formattedDate = format(new Date(selectedDate), "M월 d일 (E)", { locale: ko });
 
   const handleBackArrowClick = () => {
     navigate(-1);
@@ -83,16 +48,16 @@ const GroupCalendarPage: React.FC = () => {
         handleMiniCalendarClick={handleMiniCalendarClick}
       />
       <div className={styles.calendarSection}>
-        <Calendar type="view" scheduleData={groupCheckEvents?.groupScheduleData} currentMonth={currentMonth} setCurrentMonth={setCurrentMonth}/>
+        <Calendar type="view" scheduleData={groupCheckEvents?.groupScheduleData} currentMonth={currentMonth} setCurrentMonth={setCurrentMonth} setSelectedDate={setSelectedDate} />
       </div>
       <div className={styles.content}>
         <div className={styles.scheduleSection}>
           <div className={styles.scheduleHeaderContainer}>
-            <h1 className={styles.scheduleHeader}>1월 16일 (목)</h1>
+            <h1 className={styles.scheduleHeader}>{formattedDate}</h1>
             <EditIcon className={styles.editIcon} onClick={handleGoCreateGroupSchedule} />
           </div>
           <div className={styles.cardSection}>
-            {scheduleList.schedules.map((scheduleItem) => (
+            {groupScheduleList?.schedules.map((scheduleItem) => (
               <EventCard scheduleItem={scheduleItem} />
             ))}
           </div>
