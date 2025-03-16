48cfe238734fc40ae95c67f6f9a2a33ef02366c1
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-03-16 16:00:21 +0900

    Feat: sse 수신 데이터 eventType에 맞게 쿼리 무효화 로직 추가

--- src/api/notification/getSubscribeToSSE.ts
+++ src/api/notification/getSubscribeToSSE.ts
@@ -27,12 +27,71 @@ export const getSubscribeToSSE = async (accessToken: string, queryClient: QueryC
         break;
       }
       const chunk = decoder.decode(value, { stream: true });
-      console.log("SSE 데이터 수신: ", chunk);
-      queryClient.invalidateQueries({
-        queryKey: ["NOTIFICATION_LIST"],
-      });
+      if (chunk.startsWith("data:")) {
+        const jsonString = chunk.slice(5).trim();
+        try {
+          const parsedData = JSON.parse(jsonString);
+          console.log("parsedData: ", parsedData);
+
+          if (parsedData.eventType === "FRIEND_REQUEST") {
+            queryClient.invalidateQueries({
+              queryKey: ["RECEIVE_FRIEND_LIST"],
+            });
+          }
+
+          if (parsedData.eventType === "FRIEND_ACCEPT") {
+            ["FRIEND_LIST", "REQUEST_FRIEND_LIST"].forEach((key) =>
+              queryClient.invalidateQueries({
+                queryKey: [key],
+              }),
+            );
+          }
+
+          if (["GROUP_EXPEL", "GROUP_DELETE"].includes(parsedData.eventType)) {
+            queryClient.invalidateQueries({
+              queryKey: ["GROUP_LIST"],
+            });
+          }
+
+          if (parsedData.eventType === "GROUP_ACCEPT") {
+            ["GROUP_LIST", "GROUP_INVITE_LIST"].forEach((key) =>
+              queryClient.invalidateQueries({
+                queryKey: [key],
+              }),
+            );
+          }
+
+          if (parsedData.eventType === "GROUP_INVITE") {
+            queryClient.invalidateQueries({
+              queryKey: ["GROUP_INVITE_LIST"],
+            });
+          }
+
+          if (["GROUP_SCHEDULE_CREATE", "GROUP_SCHEDULE_DELETE"].includes(parsedData.eventType)) {
+            ["GROUP_CALENDAR_SCHEDULES", "GROUP_CHECK_EVENTS", "GROUP_SCHEDULE_LIST"].forEach(
+              (key) =>
+                queryClient.invalidateQueries({
+                  queryKey: [key],
+                }),
+            );
+          }
+
+          if (parsedData.eventType === "COMMENT") {
+            queryClient.invalidateQueries({
+              queryKey: ["COMMENT_LIST"],
+            });
+          }
+
+          queryClient.invalidateQueries({
+            queryKey: ["NOTIFICATION_LIST"],
+          });
+        } catch (error) {
+          console.error("JSON 파싱 오류: ", error);
+        }
+      }
     }
   } catch (error) {
+      console.log("SSE 연결실패")
     console.error("SSE 오류: ", error);
   }
 };
