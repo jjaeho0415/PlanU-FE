3f0b43e4fcbf896e4215a41d99ebbf44ecfa20a0
shuding0307 <suhyun2116@gmail.com>
2025-04-03 10:14:12 +0900

    sockjs수정

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => {
     define: {
       global: "window",
     },
+    optimizeDeps: {
+      include: ["sockjs-client"],
+    },
     resolve: {
       alias: [
         {
