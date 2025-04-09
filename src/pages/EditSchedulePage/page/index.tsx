5979f886d704aaf8bdf0368776cf0783887e825c
shuding0307 <suhyun2116@gmail.com>
2025-04-09 13:44:19 +0900

    .

--- src/pages/EditSchedulePage/page/index.tsx
+++ src/pages/EditSchedulePage/page/index.tsx
@@ -9,8 +9,22 @@ import TitleBox from "@components/createSchedule/TitleBox";
 import LocationBox from "@components/createSchedule/LocationBox";
 import useScheduleStore from "@store/useScheduleStore";
 import useLocationInfoStore from "@store/useLocationInfoStore";
+import useAuthStore from "@store/useAuthStore";
+import { useParams } from "react-router-dom";
 
 const EditSchedulePage: React.FC = () => {
+  const { groupId } = useParams<{ groupId: string }>();
+  // const id = groupId === "my" ? "my" : Number(groupId);
+  const { scheduleId } = useParams<{ scheduleId: string }>();
+  const { accessToken } = useAuthStore();
+  const { title, color, startDate, endDate, memo, participants } = useScheduleStore();
+
+  const handleEditConfirmClick = () => {
+    if (groupId === "my") {
+    } else {
+    }
+  };
+
   return (
     <div className={styles.Container}>
       <HasOnlyRightIconHeader
