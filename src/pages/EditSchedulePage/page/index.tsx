0089bbc6bdee091ccdc39ddecbe510a57f78f345
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-04-10 23:17:34 +0900

    제거

--- src/pages/EditSchedulePage/page/index.tsx
+++ src/pages/EditSchedulePage/page/index.tsx
@@ -36,7 +36,7 @@ const EditSchedulePage: React.FC = () => {
         <TitleBox />
         <ColorBox />
         <TimeBox />
-        <LocationBox lat={0} lng={0} name="asd" location="asd"/>
+        <LocationBox />
         <MemberBox />
         <NoteBox />
       </div>
