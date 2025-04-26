794b7432e88c6dabd280485e915ca41ac86794ee
정재호 <91364411+jjaeho0415@users.noreply.github.com>
2025-04-26 17:59:09 +0900

    style: 일정 생성하기 및 수정하기 색상 추가 모달 퍼블리싱 및 그룹 일정 생성 페이지에서의 참석자 확인 버튼 리퍼블리싱

--- src/components/createSchedule/ColorBox.tsx
+++ src/components/createSchedule/ColorBox.tsx
@@ -3,16 +3,22 @@ import styles from "./Inputs.module.scss";
 import Icon_arrow from "@assets/Icons/arrow/RightArrow.svg?react";
 
 interface props {
-  color?: string;
-  setColor?: React.Dispatch<React.SetStateAction<string>>;
+  setIsOpenChangeColorModal: React.Dispatch<React.SetStateAction<boolean>>;
+  color: string;
 }
 
-const ColorBox: React.FC<props> = () => {
+const ColorBox: React.FC<props> = ({setIsOpenChangeColorModal, color}) => {
+
+  
+  const handleChangeColorClick = () => {
+    setIsOpenChangeColorModal(true);
+  }
+
   return (
-    <div className={styles.Box}>
+    <div className={styles.Box} onClick={handleChangeColorClick}>
       <p className={styles.Title}>색상</p>
       <div className={styles.RightBox}>
-        <div className={styles.Color}></div>
+        <div className={styles.Color} style={{backgroundColor: color}}></div>
         <Icon_arrow />
       </div>
     </div>
