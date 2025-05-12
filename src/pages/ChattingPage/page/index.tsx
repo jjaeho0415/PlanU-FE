7b4aa53c8e642f560770d70a935f7f560badeafa
shuding0307 <suhyun2116@gmail.com>
2025-05-12 13:48:58 +0900

    fix: 입장 표시

--- src/pages/ChattingPage/page/index.tsx
+++ src/pages/ChattingPage/page/index.tsx
@@ -288,12 +288,18 @@ const ChattingPage: React.FC = () => {
                   chatRef.current[chat.messageId] = el;
                 }}
               >
-                <ChatBubble
-                  key={chat.messageId}
-                  message={chat}
-                  isSentByMe={userData?.username === chat.sender}
-                  type={chat.type}
-                />
+                {chat.type === 1 || chat.type === 2 ? (
+                  <ChatBubble
+                    key={chat.messageId}
+                    message={chat}
+                    isSentByMe={userData?.username === chat.sender}
+                    type={chat.type}
+                  />
+                ) : (
+                  chat.type === 5 && (
+                    <div className={styles.EnterChatRoom}>{chat.name}님이 입장했습니다.</div>
+                  )
+                )}
               </div>
             ))}
           </div>
